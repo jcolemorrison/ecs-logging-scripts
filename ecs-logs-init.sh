df42ddab59bde26f51b5476d1a2db2b5bb1e64ed
J Cole Morrison <j.cole.morrison@gmail.com>
2017-04-25 20:12:37 -0700

    update the script

--- ecs-logs-init.sh
+++ ecs-logs-init.sh
@@ -6,7 +6,7 @@ MIME-Version: 1.0
 Content-Type: text/x-shellscript; charset="us-ascii"
 #!/bin/bash
 
-# cluster to join REPLACE YOURCLUSTERNAMEHERE OBVIOUSLY
+# cluster to join
 echo ECS_CLUSTER=YOURCLUSTERNAMEHERE >> /etc/ecs/ecs.config
 
 # Install awslogs and the jq JSON parser
@@ -18,8 +18,8 @@ mv /etc/awslogs/awslogs.conf /etc/awslogs/awslogs.conf.bak
 # Inject the CloudWatch Logs configuration file contents
 cat > /etc/awslogs/awslogs.conf <<- EOF
 [general]
-state_file = /var/lib/awslogs/agent-state        
- 
+state_file = /var/lib/awslogs/agent-state
+
  # the most recent kernel messages
 [/var/log/dmesg]
 file = /var/log/dmesg
@@ -63,10 +63,6 @@ datetime_format = %Y-%m-%dT%H:%M:%SZ
 
 EOF
 
---==BOUNDARY==
-MIME-Version: 1.0
-Content-Type: text/x-shellscript; charset="us-ascii"
-#!/bin/bash
 # Set the region to send CloudWatch Logs data to (the region where the container instance is located)
 region=$(curl 169.254.169.254/latest/meta-data/placement/availability-zone | sed s'/.$//')
 sed -i -e "s/region = us-east-1/region = $region/g" /etc/awslogs/awscli.conf
@@ -86,20 +82,20 @@ start on started ecs
 script
   exec 2>>/var/log/ecs/cloudwatch-logs-start.log
   set -x
-  
+
   until curl -s http://localhost:51678/v1/metadata
   do
-    sleep 1 
+    sleep 1
   done
 
   # Grab the cluster and container instance ARN from instance metadata
   cluster=$(curl -s http://localhost:51678/v1/metadata | jq -r '. | .Cluster')
   container_instance_id=$(curl -s http://localhost:51678/v1/metadata | jq -r '. | .ContainerInstanceArn' | awk -F/ '{print $2}' )
-  
+
   # Replace the cluster name and container instance ID placeholders with the actual values
   sed -i -e "s/{cluster}/$cluster/g" /etc/awslogs/awslogs.conf
   sed -i -e "s/{container_instance_id}/$container_instance_id/g" /etc/awslogs/awslogs.conf
-  
+
   service awslogs start
   chkconfig awslogs on
 end script
